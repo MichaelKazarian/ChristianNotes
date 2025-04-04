f808a289f3f91b42931085f39cc4b798adfa895b
Kazarian Mykhailo <michael.kazarian@gmail.com>
2025-04-04 16:30:14 +0300

    New Items

---
+++ .dir-locals.el
@@ -0,0 +1,14 @@
+;;; .dir-locals.el --- 
+
+;; Copyright (C) Michael Kazarian
+;;
+;; Author: Michael Kazarian <michael.kazarian@gmail.com>
+;; Keywords: 
+;; Requirements: 
+;; Status: not intended to be distributed yet
+
+((org-mode . ((eval . (git-gutter-mode))))
+ (nil . ((indicate-empty-lines . t))))
+
+
+;;; .dir-locals.el ends here
